bbe7c863fb6e2a063c3c302c5adeeaa454b3ca35
sanjay71 <35688325+sanjay71@users.noreply.github.com>
2018-01-31 16:34:42 +0530

    sanjay01.c

---
+++ sanjay01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int  a,b,c;
+clrscr()
+printf("enter the number\n");
+scanf("%d",&a);
+printf("enter the number\n");
+scanf("%d",&b);
+c=a+b;
+printf("sum of two number is",c);
+getch();
+return0;
+}
